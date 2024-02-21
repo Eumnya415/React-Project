3a89da7e8d30fb9579d2c392c5bd7e1ac04f53e4
CHA <2124524@gmail.com>
2024-02-21 16:48:11 +0900

    Add: LifeCyle 관련 메서드 추가

--- lifecycle-react/src/LifeCycle.js
+++ lifecycle-react/src/LifeCycle.js
@@ -12,15 +12,30 @@ class LifeCycle extends Component {
     componentDidMount(){
         console.log('componentDidMount');
     }
-    
+
     // 마운트 과정에서도 호출되고, 업데이트가 시작하기 전에도 호출되는 메서드
     static getDerivedStateFromProps(nextProps, prevState){
         console.log('getDerivedStateFromProps',nextProps,prevState);
-        if(nextProps.color != prevState.color){
+        if(nextProps.color !== prevState.color){
             return{color:nextProps.color}
         }
         return null;
     }
+    getSnapshotBeforeUpdate(prevProps,prevState){
+        console.log('getSnapshotBeforeUpdate',prevProps,prevState);
+        if(prevProps.color !== this.props.color){
+        return prevProps.color;
+        }
+        return null;
+    }
+    componentDidUpdate(prevProps,prevState,snapshot){
+        console.log('componentDidUpdate',prevProps,prevState,snapshot);
+    }
+    shouldComponentUpdate(nextProps,nextState){
+        console.log('shouldComponentUpdate',nextProps,nextState);
+        return nextProps.color === 'green' ? false : true;
+    }
+
     render() {
         console.log('render');
         return (
