b994e06dd7b2d5e2a78c0c608ed1d76b502b67f6
CHA <2124524@gmail.com>
2024-02-21 15:50:43 +0900

    Feat: App 초기 실행 화면 UI 조정

--- lifecycle-react/src/LifeCycle.js
+++ lifecycle-react/src/LifeCycle.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
 class LifeCycle extends Component {
+    state = {
+        color:this.props.color
+    }
     render() {
+        console.log('render');
         return (
             <div>
-                
+                <h1 style={{color:this.props.color}}>
+                    props : {this.props.color} <br />
+                    state : {this.state.color}
+                </h1>
             </div>
         );
     }
