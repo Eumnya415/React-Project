8e0e04ecfde13a537b8641af3065138f41c6ee34
CHA <2124524@gmail.com>
2024-02-23 14:30:08 +0900

    Add: todos 배열에 새 객체를 추가하는 코드를 App.js에 먼저 추가

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -8,12 +8,19 @@ const App = () => {
     [
       {id:1,text:'일정1',checked:true},
       {id:2,text:'일정2',checked:true},
-      {id:3,text:'일정3',checked:false}
+      {id:3,text:'일정3',checked:true}
     ]
   );
+  const nextId = useRef(4); // id 관리용
+  const onInsert = useCallback(
+    (text) => {
+    const nextTodo = {id:nextId.current,text:text,checked:false};
+    setTodos(todos.concat(nextTodo));
+    nextId.current = nextId.current + 1; // id 4 -> 5
+    },[todos]);
   return (
       <TodoTemplate>
-        <TodoInsert/>
+        <TodoInsert onInsert={onInsert} />
         <TodoList todos={todos}/>
       </TodoTemplate>
   );
