a0672f939cc7cf80d315f03ba48f0bc4d529797f
CHA <2124524@gmail.com>
2024-02-23 13:20:36 +0900

    Feat: 항목 추가 기능 구현

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import './TodoInsert.css';
 import {MdAdd} from 'react-icons/md';
 
 const TodoInsert = () => {
+    const [value,setValue] = useState('');
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    },[]);
     return (
         <form className='TodoInsert'>
-            <input placeholder='할 일을 입력하세요.'/>
+            <input placeholder='할 일을 입력하세요.' onChange={onChange}/>
             <button type='submit'><MdAdd /></button>
         </form>
     );
