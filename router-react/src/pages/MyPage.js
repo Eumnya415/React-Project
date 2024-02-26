43f5342c8a2d07c66913720e71cceb8f22fc791f
CHA <2124524@gmail.com>
2024-02-26 16:31:22 +0900

    Navigate 컴포넌트 설정

---
+++ router-react/src/pages/MyPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const MyPage = () => {
+    const isLogin = false;
+    if(!isLogin) {
+        return <Navigate to='/login' replace={true} />;
+    }
+    return (
+        <div>
+            마이 페이지
+        </div>
+    );
+};
+
+export default MyPage;
