3d1ebc63e5e798af79f27bde1460ee11cfdb2cc3
CHA <2124524@gmail.com>
2024-02-26 15:41:50 +0900

    Add: 중첩된 라우트 설정

--- router-react/src/App.js
+++ router-react/src/App.js
@@ -5,15 +5,19 @@ import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Articles from './pages/Articles';
 import Article from './pages/Article';
+import Layout from './pages/Layout';
 
 const App = () => {
   return (
     <Routes>
+      <Route element={<Layout/>}>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/profiles/:id' element={<Profile/>}/>
-      <Route path='/articles' element={<Articles/>}/>
-      <Route path='/articles/:id' element={<Article/>}/>
+      </Route>
+      <Route path='/articles' element={<Articles/>}>
+      <Route path=':id' element={<Article/>}/>
+      </Route>
     </Routes>
   );
 };
