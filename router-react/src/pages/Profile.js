664bcfb29e613fe4d59b880a770ccc59f14c2aaa
CHA <2124524@gmail.com>
2024-02-26 14:49:07 +0900

    삼항연산자로 존재하지 않는 프로필일 때 코드 추가

--- router-react/src/pages/Profile.js
+++ router-react/src/pages/Profile.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+const data = {
+    user1 : {
+        name : '차소영',
+        desc : '주니어 개발자'
+    },
+    user2 : {
+        name : '천재교육',
+        desc : '주니어 개발자'
+    },
+}
+
 const Profile = () => {
     const params = useParams();
-    const profile = params.id;
+    const profile = data[params.id];
     return (
-        <div>
-            <h1>사용자 프로필</h1>
-            <p>profile id : {profile}</p>
-        </div>
+            <div>
+                <h1>사용자 프로필</h1>
+                {profile ? (<div><h2>{profile.name}</h2><p>{profile.desc}</p></div>) : (<p>존재하지 않는 프로필입니다.</p>)}
+            </div>
     );
 };
 
