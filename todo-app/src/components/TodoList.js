6463af72f9417adaf77bbcb56eb7ec28e52f8f40
CHA <2124524@gmail.com>
2024-02-26 10:24:17 +0900

    react-virtualized 수정

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import './TodoList.css';
 import TodoListItem from './TodoListItem';
+import {List} from 'react-virtualized';
 
 const TodoList = ({todos, onRemove, onToggle}) => {
+    const rowRender = useCallback(
+        ({index,key,style}) => {
+        const todo = todos[index];
+        return (
+            <TodoListItem todo={todo} key={key} onRemove={onRemove} onToggle={onToggle} style={style}/>
+        );
+    },[todos,onRemove,onToggle]);
+    return (
+        <List
+        className='TodoList' width={512} height={513} rowCount={todos.length}
+        rowHeight={57} rowRenderer={rowRender} list={todos} style={{outline:'none'}}
+        />
+    );
+};
+
+export default React.memo(TodoList);
+
+
+
+/*
+const TodoList = ({todos, onRemove, onToggle}) => {
+    const todo = todos[index];
     return (
         <div className='TodoList'>
             {
@@ -13,3 +36,4 @@ const TodoList = ({todos, onRemove, onToggle}) => {
 };
 
 export default React.memo(TodoList);
+*/
