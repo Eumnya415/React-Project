f070e9fbe226d83493a7acf5458677c7b1324b21
CHA <2124524@gmail.com>
2024-02-26 16:32:04 +0900

    Add: NavLink 설정

--- router-react/src/pages/Articles.js
+++ router-react/src/pages/Articles.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 
 const Articles = () => {
+    const activeStyle = {color:'green',fontSize:21};
     return (
         <div>
             <Outlet/>
-            <Link to='/articles/1'>게시글1</Link>
-            <Link to='/articles/2'>게시글2</Link>
-            <Link to='/articles/3'>게시글3</Link>
+            <NavLink to='/articles/1' style={({isActive}) => (isActive ? activeStyle : undefined)}>게시글1</NavLink> |
+            <NavLink to='/articles/2' style={({isActive}) => (isActive ? activeStyle : undefined)}>게시글2</NavLink> |
+            <NavLink to='/articles/3' style={({isActive}) => (isActive ? activeStyle : undefined)}>게시글3</NavLink>
         </div>
     );
 };
