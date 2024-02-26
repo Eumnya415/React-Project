7e43c299e36dbb5dcf1b1dc25d960a9768510bf5
CHA <2124524@gmail.com>
2024-02-26 14:35:51 +0900

    Test: 테스트용 코드 수정

--- router-react/src/pages/Profile.js
+++ router-react/src/pages/Profile.js
@@ -3,11 +3,11 @@ import { useParams } from 'react-router-dom';
 
 const Profile = () => {
     const params = useParams();
-    const profile = data[params.id];
+    const profile = params.id;
     return (
         <div>
             <h1>사용자 프로필</h1>
-            {profile ? (<div><h2>{profile.names}</h2><p>{profile.desc}</p></div>) : (<p>존재하지 않는 프로필입니다.</p>)}
+            <p>profile id : {profile}</p>
         </div>
     );
 };
