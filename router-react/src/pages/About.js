3c13a9bec5065603fce7de694946cc0eb698f818
CHA <2124524@gmail.com>
2024-02-26 15:24:59 +0900

    About 컴포넌트 - useSearchParams

--- router-react/src/pages/About.js
+++ router-react/src/pages/About.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useSearchParams } from 'react-router-dom';
 
 const About = () => {
     const location = useLocation();
+    const [searchParams,setSearchParams] = useSearchParams();
+    const detail = searchParams.get('detail');
+    const mode = searchParams.get('mode');
     return (
         <div>
             <h1>소개</h1>
             <p>리액트 라우터를 사용해 보는 프로젝트</p>
             <p>쿼리스트링 : {location.search}</p>
+            <p>detail : {detail}</p>
+            <p>mode : {mode}</p>
         </div>
     );
 };
