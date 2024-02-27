e8f993c0cf4e24a22b8ebfc81508b08562a7dc60
CHA <2124524@gmail.com>
2024-02-27 16:42:51 +0900

    Feat: News 창 업로드

--- news-viewer-react/src/components/News.js
+++ news-viewer-react/src/components/News.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import NewsList from './NewsList';
 
 const News = () => {
+    const [category,setCategory] = useState('all');
+    const onSelect = useCallback((e,category) => {
+        setCategory(category); 
+        e.preventDefault(); // 이중 클릭 방지
+    },[] // 한 번만 마운트
+    );
   return (
     <section>
       <ul className="nav nav-tabs">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">
+          <a onClick={(e) => onSelect(e,'all')} className={category == 'all' ? 'nav-link active': 'nav-link'} aria-current="page" href="#">
             전체보기
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">
+          <a onClick={(e) => onSelect(e,'business')} className={category == 'business' ? 'nav-link active': 'nav-link'} href="#">
             비즈니스
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">
+          <a onClick={(e) => onSelect(e,'entertainment')} className={category == 'entertainment' ? 'nav-link active': 'nav-link'} href="#">
             엔터테인먼트
           </a>
         </li>
@@ -23,56 +30,9 @@ const News = () => {
       <div className="album py-5 bg-light">
         <div className="container">
           <div className="row">
-            <div className="col-md-6">
-              <div className="card mb-4 box-shadow">
-                <img
-                  className="card-img-top"
-                  src="https://placehold.co/160X80"
-                  alt="Card image cap"
-                />
-                <div className="card-body">
-                  <h4>title</h4>
-                  <p className="card-text">
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
-                  </p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <a href="#" className="btn btn-sm btn-outline-secondary">
-                        View
-                      </a>
-                    </div>
-                    <small className="text-muted">NAVER</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-6">
-              <div className="card mb-4 box-shadow">
-                <img
-                  className="card-img-top"
-                  src="https://placehold.co/160X80"
-                  alt="Card image cap"
-                />
-                <div className="card-body">
-                  <h4>title</h4>
-                  <p className="card-text">
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
-                  </p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <a href="#" className="btn btn-sm btn-outline-secondary">
-                        View
-                      </a>
-                    </div>
-                    <small className="text-muted">NAVER</small>
-                  </div>
-                </div>
-              </div>
-            </div>
+
+            <NewsList category={category}/>
+          
           </div>
         </div>
       </div>
