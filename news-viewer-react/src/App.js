c502c945d0fc4fc272d42742c1a5f7e7e7557894
CHA <2124524@gmail.com>
2024-02-27 13:44:46 +0900

    news api url 삽입

--- news-viewer-react/src/App.js
+++ news-viewer-react/src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from '../node_modules/axios/index';
 
 const App = () => {
   const [data,setData] = useState(null);
-  const onClick = () => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/1')
-    .then((response) => {
+  const onClick = async () => {
+    const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=822dc0d8a8294d50b70237bbe4bb4408');
       setData(response);
-    });
+  };
   return (
     <div>
       <button onClick={onClick}>불러오기</button>
+      <div>
+      {data && <textarea value={JSON.stringify(data,null,2)} />}
+      </div>
     </div>
   );
 };
