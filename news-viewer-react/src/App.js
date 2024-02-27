651e65eb9acf088d94442e2ea5450bd75603577d
CHA <2124524@gmail.com>
2024-02-27 16:44:01 +0900

    Feat: Route 컴포넌트 설정

--- news-viewer-react/src/App.js
+++ news-viewer-react/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Route,Routes } from '../node_modules/react-router-dom/dist/index';
 import Layout from './components/Layout';
 import Home from './components/Home';
+import News from './components/News';
+import NotFound from './components/NotFound';
 
 const App = () => {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path='/' element={<Home/>} />
+        <Route path='/news' element={<News/>} />
+        <Route path='*' element={<NotFound/>}/>
       </Route>
     </Routes>
   );
