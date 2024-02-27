a27b4bad14be1b49f8e3de5bacd64a5186849487
CHA <2124524@gmail.com>
2024-02-27 16:42:04 +0900

    Feat: 헤더에서 home, 뉴스 버튼 링크 연결

--- news-viewer-react/src/components/Header.js
+++ news-viewer-react/src/components/Header.js
@@ -1,16 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
       <h5 className="my-0 mr-md-auto font-weight-bold">News API</h5>
       <nav className="my-2 my-md-0 mr-md-3">
-        <a className="p-2 text-dark active" href="#">
-          HOME
-        </a>
-        <a className="p-2 text-dark" href="#">
-          뉴스
-        </a>
+        <NavLink className={({isActive}) => (isActive ? "p-2 text-dark active" : "p-2 text-dark")} to="/">HOME</NavLink>
+        <NavLink className={({isActive}) => (isActive ? "p-2 text-dark active" : "p-2 text-dark")} to="/news">뉴스</NavLink>
       </nav>
     </div>
   );
